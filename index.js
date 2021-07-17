eeea4f982b9ad780fd21598ea24df8c89db0a5ff
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 06:21:50 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function myfun(num){
+    var i=0
+    while(i<num){
+        i++
+        console.log(i)
+    }
+}
+myfun(5)
+
+let cars=['Mustang','Bmw','porche','Lambo']
+var i=0
+while(i<cars.length){
+console.log(cars[i]) 
+i++
+}
